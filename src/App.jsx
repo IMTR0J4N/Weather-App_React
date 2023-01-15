8c94eab729298d00efbb35bfb5134fcc543cd02e
IMTR0J4NV2 <jjorgebeau@edenschool.fr>
2023-01-15 02:18:44 +0100

    Add Top Buttons and start code Inputs components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import UilReact from '@iconscout/react-unicons/icons/uil-react';
+import TopButtons from './components/TopButtons';
+import Inputs from './components/Inputs';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-        
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+      <TopButtons />
+      <Inputs />
     </div>
   )
 }
---
+++ src/components/TopButtons.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default function TopButtons() {
+  const cities = [
+    {
+      id: 1,
+      title: "Paris",
+    },
+    {
+      id: 2,
+      title: "Madrid",
+    },
+    {
+      id: 3,
+      title: "Lisbonne",
+    },
+    {
+      id: 4,
+      title: "Tokyo",
+    },
+    {
+      id: 5,
+      title: "Londres",
+    },
+  ];
+
+  return (
+    <div className="flex items-center justify-around my-6">
+        { cities.map((city) => {
+            return <button key={city.id} className="text-white text-lg font-medium">{city.title}</button>
+        }) }
+    </div>
+  );
+}
