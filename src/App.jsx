18bbdba341587b63f07cc258f7d174a05e00b7b1
IMTR0J4NV2 <jjorgebeau@edenschool.fr>
2023-01-15 18:33:34 +0100

    Edit forecast components

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,8 @@ function App() {
       <TimeAndLocation />
       <TemperatureAndDetails />
 
-      <Forecast />
+      <Forecast title="hourly forecast"/>
+      <Forecast title="daily forecast"/>
     </div>
   )
 }
--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
 
-function Forecast() {
+function Forecast({title}) {
   return (
-    <div>Forecast</div>
-  )
+    <div>
+      <div className="flex items-center justify-start my-6">
+        <p className="text-white font-medium uppercase">{title}</p>
+      </div>
+      <hr className="my-2" />
+
+      <div className="flex flex-row items-center justify-between text-white">
+        <div className="flex flex-col items-center justify-center">
+          <p className="font-light text-sm">4:30 PM</p>
+          <img
+            src="http://openweathermap.org/img/wn/01d@2x.png"
+            className="w-12 my-1"
+          />
+          <p className="font-medium">22°</p>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <p className="font-light text-sm">4:30 PM</p>
+          <img
+            src="http://openweathermap.org/img/wn/01d@2x.png"
+            className="w-12 my-1"
+          />
+          <p className="font-medium">22°</p>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <p className="font-light text-sm">4:30 PM</p>
+          <img
+            src="http://openweathermap.org/img/wn/01d@2x.png"
+            className="w-12 my-1"
+          />
+          <p className="font-medium">22°</p>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <p className="font-light text-sm">4:30 PM</p>
+          <img
+            src="http://openweathermap.org/img/wn/01d@2x.png"
+            className="w-12 my-1"
+          />
+          <p className="font-medium">22°</p>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <p className="font-light text-sm">4:30 PM</p>
+          <img
+            src="http://openweathermap.org/img/wn/01d@2x.png"
+            className="w-12 my-1"
+          />
+          <p className="font-medium">22°</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Forecast
+export default Forecast;
