5563ad9ef325274960dada7eedb0bae49bb8e51b
IMTR0J4NV2 <jjorgebeau@edenschool.fr>
2023-01-15 17:43:48 +0100

    Finish code Time and Location components and add Poppins font family

--- src/components/TimeAndLocation.jsx
+++ src/components/TimeAndLocation.jsx
@@ -4,10 +4,15 @@ function TimeAndLocation() {
   return (
     <div>
         <div className='flex items-center justify-center my-6'>
+            <p className='text-white text-xl font-extralight'>
+              Tuesday, 31 May 2022 | Local Time: 12:46 PM              
+            </p>
+        </div>
 
-            <div className='text-white text-xl font-extralight'></div>
-
-
+        <div className='flex items-center justify-center my-3'>
+          <p className='text-white text-3xl font-medium'>
+            Berlin, DE
+          </p>
         </div>
     </div>
   )
