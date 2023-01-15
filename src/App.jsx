e7d50f131cccfdd248c2ab72788e55e4810de0cc
IMTR0J4NV2 <jjorgebeau@edenschool.fr>
2023-01-15 18:27:29 +0100

    Start code Forecast components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import TopButtons from './components/TopButtons';
 import Inputs from './components/Inputs';
 import TimeAndLocation from './components/TimeAndLocation';
 import TemperatureAndDetails from './components/TemperatureAndDetails';
+import Forecast from './components/Forecast';
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
 
       <TimeAndLocation />
       <TemperatureAndDetails />
+
+      <Forecast />
     </div>
   )
 }
