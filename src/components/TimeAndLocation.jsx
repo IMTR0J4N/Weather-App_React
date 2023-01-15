0857b2235771de8395b81e9b7e638d96f434661c
IMTR0J4NV2 <jjorgebeau@edenschool.fr>
2023-01-15 02:49:10 +0100

    Start code Time and Location components

--- src/components/TimeAndLocation.jsx
+++ src/components/TimeAndLocation.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function TimeAndLocation() {
   return (
-    <div>TimeAndLocation</div>
+    <div>
+        <div className='flex items-center justify-center my-6'>
+
+            <div className='text-white text-xl font-extralight'></div>
+
+
+        </div>
+    </div>
   )
 }
 
