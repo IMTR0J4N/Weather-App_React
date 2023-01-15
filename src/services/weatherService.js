ccdac8d5bdc145c3899f22503fb7e22193cce360
IMTR0J4NV2 <jjorgebeau@edenschool.fr>
2023-01-16 00:52:21 +0100

    Add env variables and code weather services

---
+++ src/services/weatherService.js
@@ -0,0 +1,69 @@
+const config = import.meta.env;
+
+const API_KEY = config.VITE_API_KEY;
+const BASE_URL = "https://api.openweathermap.org/data/2.5/";
+
+const getWeatherData = (infoType, searchParams) => {
+  const url = new URL(`${BASE_URL}/${infoType}`);
+  url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+
+  return fetch(url)
+    .then((res) => res.json())
+    .then((data) => data);
+};
+
+const formatCurrentWeather = (data) => {
+  const {
+    coord: { lat, lon },
+    main: { temp, feels_like, temp_min, temp_max, humidity },
+    name,
+    dt,
+    sys: { country, sunrise, sunset },
+    weather,
+    wind: { speed },
+  } = data;
+
+  const { main: details, icon } = weather[0];
+
+  return {
+    lat,
+    lon,
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    humidity,
+    name,
+    dt,
+    country,
+    sunrise,
+    sunset,
+    weather,
+    speed,
+  };
+};
+
+const formatForecastWeather = (data) => {
+    let { timezone, daily, hourly } = data;
+    daily = daily.slice(1,6).map()
+}
+
+const getFormattedWeatherData = async (searchParams) => {
+  const formattedCurrentWeather = await getWeatherData(
+    "weather",
+    searchParams
+  ).then(formatCurrentWeather);
+
+  const { lat, lon } = formattedCurrentWeather;
+
+  const formattedForecastWeather = await getWeatherData("onecall", {
+    lat,
+    lon,
+    exclude: "current,minutely,alerts",
+    units: searchParams.units,
+  }).then(formatFor);
+
+  return formattedCurrentWeather;
+};
+
+export default getFormattedWeatherData;
